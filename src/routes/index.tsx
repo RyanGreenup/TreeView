ae472b1858842e52791a2d9061746eedcd9a2766
Ryan G <exogenesis@protonmail.com>
2025-07-12 23:14:37 +1000

    Fresh Start

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,7 @@ import { TreeView } from "~/components/TreeView";
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-8">
-      <h1 class="text-4xl text-sky-700 font-bold mb-8">Zag-JS TreeView Example</h1>
-      <TreeView />
+      <h1 class="text-4xl text-sky-700 font-bold mb-8">TreeView Example</h1>
       <p class="mt-8">
         <A href="/about" class="text-sky-600 hover:underline">
           About Page
